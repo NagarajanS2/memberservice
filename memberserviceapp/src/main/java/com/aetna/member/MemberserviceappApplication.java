1a452211f159f760265af6037b0e5cbeea226ee2
a443191 <a443191@ip-192-168-1-105.ec2.internal>
2020-04-21 15:55:46 -0400

    First MemberService Commit

---
+++ memberserviceapp/src/main/java/com/aetna/member/MemberserviceappApplication.java
@@ -0,0 +1,13 @@
+package com.aetna.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberserviceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberserviceappApplication.class, args);
+	}
+
+}
